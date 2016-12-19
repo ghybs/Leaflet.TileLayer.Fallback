5ee7983223818b18bc68e6bff389111a49334a85
ghybs <ghybs1@gmail.com>
2016-12-19 09:22:45 +0400

    Fix #4 UMD wrapper
    Now side effect only.

--- build/copyright.js
+++ build/copyright.js
@@ -1,5 +1,5 @@
 /*!
- Leaflet.TileLayer.Fallback {VERSION}
- (c) {YEAR} Boris Seang
- License Apache-2.0
+ {NAME} {VERSION}
+ (c) {YEAR} {AUTHOR}
+ License {LICENSE}
  */
--- build/umd.template.js
+++ build/umd.template.js
@@ -3,24 +3,21 @@ module.exports = {
     ///////////////////////////
     before:
     ///////////////////////////
-'(function (root, factory) {\n' +
-'    if (typeof define === "function" && define.amd) {\n' +
-'        define(["leaflet"], function (L) {\n' +
-'            return (root.L.TileLayer.Fallback = factory(L));\n' +
-'        });\n' +
-'    } else if (typeof module === "object" && module.exports) {\n' +
-'        module.exports = factory(require("leaflet"));\n' +
-'    } else {\n' +
-'        root.L.TileLayer.Fallback = factory(root.L);\n' +
-'    }\n' +
-'}(this, function (L) {\n\n',
+    '(function (root, factory) {\n' +
+    '    if (typeof define === "function" && define.amd) {\n' +
+    '        define(["leaflet"], factory);\n' +
+    '    } else if (typeof module === "object" && module.exports) {\n' +
+    '        factory(require("leaflet"));\n' + // Side effect only even in CommonJS
+    '    } else {\n' +
+    '        factory(root.L);\n' +
+    '    }\n' +
+    '}(this, function (L) {\n\n',
 
 
     ///////////////////////////
     after:
     ///////////////////////////
-'\n    return FallbackTileLayer;\n' + // Must be the same identifier as in src!
-'\n' +
-'}));\n'
+    '\n' +
+    '}));\n'
 
 };
